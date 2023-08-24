73d5acf7970becd3d69c45925bbece49f9503027
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-08-24 15:12:20 -0500

    Ruta para buscar libros por su título

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/buscar', methods=['POST'])
+def buscar():
+    titulo = request.form.get('title')
+
+    datos = {
+        'titulo': titulo
+    }
+    
+    return jsonify(datos)
