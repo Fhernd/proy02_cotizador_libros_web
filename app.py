3c31c4dc5be46138edeeb513a6786d342446a97e
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-08-25 14:51:23 -0500

    Extracción de datos relevantes de libros en BuscaLibre

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask, jsonify, render_template, request
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -53,5 +54,42 @@ def buscar_busca_libre_libreria(driver, titulo):
 
     # Presione la tecla Enter:
     q.send_keys(Keys.ENTER)
+
+    # Esperar 5 segundos:
+    time.sleep(5)
+
+    # Realizar la búsqueda con este selector CSS "div.box-producto":
+    divs = driver.find_elements(By.CSS_SELECTOR, 'div.box-producto')
+
+    libros = []
+
+    for div in divs:
+        # Extraer el valor del atribut "src" de la primera etiqueta img:
+        img = div.find_element(By.TAG_NAME, 'img')
+        src = img.get_attribute('src')
+
+        # Extraer el texto de este selector CSS 'h3.nombre':
+        nombre = div.find_element(By.CSS_SELECTOR, 'h3.nombre')
+        nombre = nombre.text
+
+        # Extraer el texto del selector div.autor:
+        autor = div.find_element(By.CSS_SELECTOR, 'div.autor')
+        autor = autor.text
+
+        # Extraer el texto del div con clases 'autor color-dark-gray metas hide-on-hover':
+        otros_datos = div.find_element(By.CSS_SELECTOR, 'div.autor.color-dark-gray.metas.hide-on-hover')
+        otros_datos = otros_datos.text
+
+        # Extraer el texto del elemento p con clases 'precio-ahora hide-on-hover margin-0 font-size-medium':
+        precio = div.find_element(By.CSS_SELECTOR, 'p.precio-ahora.hide-on-hover.margin-0.font-size-medium')
+        precio = precio.text
+
+        libros.append({
+            'src': src,
+            'nombre': nombre,
+            'autor': autor,
+            'otros_datos': otros_datos,
+            'precio': precio
+        })
     
-    return driver.page_source
+    return libros
